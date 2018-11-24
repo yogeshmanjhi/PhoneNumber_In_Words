6f26c57f6c91af5143c100631f8609753f4c1fe3
yogeshmanjhi <yogeshmanjhi93@gmail.com>
2018-11-24 17:24:06 +0530

    calculating the combinations

--- phonenumber/app/controllers/phones_controller.rb
+++ phonenumber/app/controllers/phones_controller.rb
@@ -18,6 +18,21 @@ class PhonesController < ApplicationController
     File.foreach( file_path ) do |word|
       dictionary[word.length] << word.chop.to_s.downcase
     end
+    keys = phone.chars.map{|e|letters[e]}
+    temp = {}
+    max_index = keys.length - 1 # max_index
+    #Looping through all letters and get matching records with dictionary
+    for i in (2..max_index - 2)
+      array1 = keys[0..i]
+      next if array1.length < 3
+      array2 = keys[i + 1..max_index]
+      next if array2.length < 3
+      combination1 = array1.shift.product(*array1).map(&:join) #calculating the product of arrays 
+      next if combination1.nil?
+      combination2 = array2.shift.product(*array2).map(&:join)
+      next if combination2.nil?
+      temp[i] = [(combination1 & dictionary[i+2]), (combination2 & dictionary[max_index - i +1])] #common values calculation
+    end
 
   end
 end
