2e1da5f8bab006c6ee7a940f5fb53b929dc47e74
yogeshmanjhi <yogeshmanjhi93@gmail.com>
2018-11-24 15:55:23 +0530

    getting values from dictinary and storing in array

--- phonenumber/app/controllers/phones_controller.rb
+++ phonenumber/app/controllers/phones_controller.rb
@@ -9,5 +9,15 @@ class PhonesController < ApplicationController
     return [] if phone.nil? || phone.length!=10 || phone.split('').select{|d|d.to_i ==0 || d.to_i ==1}.length >0
     #creating mapping as given in the question
     letters = {"2" => ["a", "b", "c"],"3" => ["d", "e", "f"],"4" => ["g", "h", "i"],"5" => ["j", "k", "l"],"6" => ["m", "n", "o"],"7" => ["p", "q", "r", "s"],"8" => ["t", "u", "v"],"9" => ["w", "x", "y", "z"]}
+    # getting all the values from dictionary and storing in array 
+    dictionary = {}
+    for i in (1..30)
+      dictionary[i] = []
+    end
+    file_path = Rails.root.join("public", "dictionary.txt")
+    File.foreach( file_path ) do |word|
+      dictionary[word.length] << word.chop.to_s.downcase
+    end
+
   end
 end
