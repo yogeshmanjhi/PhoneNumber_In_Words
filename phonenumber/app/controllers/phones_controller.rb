383fb62b88945ef008e2fbe43729ae7633739d67
yogeshmanjhi <yogeshmanjhi93@gmail.com>
2018-11-24 13:35:46 +0530

    getting the phone from params and checking validity

--- phonenumber/app/controllers/phones_controller.rb
+++ phonenumber/app/controllers/phones_controller.rb
@@ -3,5 +3,10 @@ class PhonesController < ApplicationController
 	end
 
 	def create
+		#getting the phone number from params
+		phone=params[:phone][:phone]
+		#checking the validity of phone 
+		return [] if phone.nil? || phone.length!=10 || phone.split('').select{|d|d.to_i ==0 || d.to_i ==1}.length >0 
+		
 	end
 end
